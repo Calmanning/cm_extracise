4dab197f8d8e10c83215fa3387370012219a3a8b
That Guy Cal <calmanning@gmail.com>
2021-02-06 16:32:06 -0800

    finished fron-end js. May need to re-arrange some of the AJAX...just for my own style's sake.

--- public/script.js
+++ public/script.js
@@ -1,41 +1,91 @@
-function getWorkouts () {
+function getWorkouts() {
     $("#results").empty();
-    $getJSON("/all", function(data) {
-        for (const i = 0; i< data.length; i++) {
+    $getJSON("/all", function (data) {
+        for (const i = 0; i < data.length; i++) {
             $("#results").prepend("<p class='data-entry' data-id=" + data[i]._id + "><span class='dataTitle' data-id=" +
-            data[i]._id + ">" + data[i].title + "</span><span class='delete'>X</span></p>");
+                data[i]._id + ">" + data[i].title + "</span><span class='delete'>X</span></p>");
         }
     })
 }
 
 getWorkouts();
 
-$(document).on("click", "#newExercise", function() {
+$(document).on("click", "#newExercise", function () {
     $.ajax({
         type: "POST",
         dataType: "json",
-        url: "/newExercise"
+        url: "/newExercise",
         data: {
             title: $("#workoutTitle").val(),
             exercise: $("#activity").val(),
             created: Date.now()
         }
-    }).then(function(data) {
+    }).then(function (data) {
         $("#results").prepend("<p class='data-entry' data-id=" + data._id + "><span class='dataTitle' data-id=" +
-        data._id + ">" + data.title + "</span><span class='delete'>X</span></p>");
-        // Clear the note and title inputs on the page
-        $("#note").val("");
+            data._id + ">" + data.title + "</span><span class='delete'>X</span></p>");
+        // Clear the activity and title inputs on the page
+        $("#activity").val("");
         $("#title").val("");
     })
 })
 
-$("#clear-all").on("click", function() {
+$("#clear-all").on("click", function () {
     $.ajax({
         type: "DELTE",
-        datatype: "json",
+        dataType: "json",
         url: "/clear",
-        success: function() {
-            $("#results")
+        success: function () {
+            $("#results").empty()
+        }
+    })
+})
+
+$(document).on("click", ".delete", function () {
+    const exerciseId = $(this).parent();
+
+    $.ajax({
+        type: "DELETE",
+        url: "/delete/" + exerciseId.attr("data-id"),
+        success: function (response) {
+            exerciseId.remove()
+            $("#activity").val("");
+            $("#workoutTitle").val("");
+            $("#action-button").html("<buttton id='newExercise'> Submit </button>")
+        }
+    })
+
+})
+
+$(document).on("click", ".dataTitle", function () {
+    const updatingWorkout = $(this);
+
+    $.ajax({
+        type: "GET",
+        url: "/find/" + updatingWorkout.attr("data-id"),
+        success: function (data) {
+            $("#activity").val(data.activity);
+            $("#workoutTitle").val(data.activity);
+            $("#action-button").html("<buttton id='update' data-id='" + data._id + "'> Update</button>")
+        }
+    })
+})
+
+$(document).on("click", "#update", function () {
+    const updatingId = $(this);
+
+    $.ajax({
+        type: "GET",
+        url: "/update/",
+        dataType: "json",
+        data: {
+            title: $("#workoutTitle").val(),
+            activity: $("#activity").val()
+        },
+        success: function (data) {
+            $("#activity").val("")
+            $("#workoutTitle").val("")
+            $("#action-button").html("<button id='newExercise'> Submit</button>")
+            getWorkouts();
         }
     })
 })
